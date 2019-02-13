40c56e42660505a11197ba3f3fbc84c5d4a6e76d
Oliver <0liver@users.noreply.github.com>
2019-02-13 11:35:29 +0100

    Update RunBoxstarterInstall.bat
    changed url to my own repo

--- RunBoxstarterInstall.bat
+++ RunBoxstarterInstall.bat
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://raw.githubusercontent.com/jsturtevant/chocolatey-recipe/master/chocolatey-recipe.txt 
+START http://boxstarter.org/package/url?https://raw.githubusercontent.com/0liver/chocolatey-recipe/master/chocolatey-recipe.txt 
