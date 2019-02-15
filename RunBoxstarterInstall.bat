02385ac43c1da240200341c5de44b2f6629d1179
Oliver <0liver@users.noreply.github.com>
2019-02-15 14:00:12 +0100

    update URL to https

--- RunBoxstarterInstall.bat
+++ RunBoxstarterInstall.bat
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://raw.githubusercontent.com/0liver/chocolatey-recipe/master/chocolatey-recipe.txt 
+START https://boxstarter.org/package/url?https://raw.githubusercontent.com/0liver/chocolatey-recipe/master/chocolatey-recipe.txt 
